3890c60fd7122917a9d1ce4bc62ae5d37828ab33
Misha <mishabeheka@gmail.com>
2020-04-10 17:51:54 +0300

    Added fetchType to model of User

--- src/main/java/com/dev/task/exchange_rate/model/User.java
+++ src/main/java/com/dev/task/exchange_rate/model/User.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,6 +23,6 @@ public class User {
     private String email;
     @Column(name = "password")
     private String password;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles = new HashSet<>();
 }
