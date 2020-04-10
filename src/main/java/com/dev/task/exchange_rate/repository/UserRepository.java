cd7969d04d8a753e251f6156655e900e76499abb
Misha <mishabeheka@gmail.com>
2020-04-10 16:08:50 +0300

    Added annotations to repositories

--- src/main/java/com/dev/task/exchange_rate/repository/UserRepository.java
+++ src/main/java/com/dev/task/exchange_rate/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.dev.task.exchange_rate.repository;
 
 import com.dev.task.exchange_rate.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
