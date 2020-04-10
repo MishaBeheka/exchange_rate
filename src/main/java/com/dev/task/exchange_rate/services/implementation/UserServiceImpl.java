06f05032a6697786646f5ebd5b74fcf4438a49ad
Misha <mishabeheka@gmail.com>
2020-04-10 16:13:42 +0300

    Created UserServiceImpl

---
+++ src/main/java/com/dev/task/exchange_rate/services/implementation/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.dev.task.exchange_rate.services.implementation;
+
+import com.dev.task.exchange_rate.model.User;
+import com.dev.task.exchange_rate.repository.UserRepository;
+import com.dev.task.exchange_rate.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+}
