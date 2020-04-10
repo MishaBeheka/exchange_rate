ea078fbf85a393af5bbd71b9caee8d92d5c67924
Misha <mishabeheka@gmail.com>
2020-04-10 16:04:17 +0300

    Added roles to model of user

--- src/main/java/com/dev/task/exchange_rate/model/User.java
+++ src/main/java/com/dev/task/exchange_rate/model/User.java
@@ -1,10 +1,13 @@
 package com.dev.task.exchange_rate.model;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import lombok.Data;
 
@@ -19,4 +22,6 @@ public class User {
     private String email;
     @Column(name = "password")
     private String password;
+    @ManyToMany
+    private Set<Role> roles = new HashSet<>();
 }
