19b820d80ff76ccc63ac125a291e30108496f603
Misha <mishabeheka@gmail.com>
2020-04-11 05:54:23 +0300

    Corrected GettingExchangeRateController

--- src/main/java/com/dev/task/exchange_rate/controllers/GettingExchangeRateController.java
+++ src/main/java/com/dev/task/exchange_rate/controllers/GettingExchangeRateController.java
@@ -13,7 +13,6 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.context.annotation.RequestScope;
 
 @Controller
 public class GettingExchangeRateController {
